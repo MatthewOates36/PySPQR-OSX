1cb3caef8ddaa3d21f865b26e49f26d0efa62442
Matthew Oates <uni.oates@gmail.com>
2024-11-03 02:42:24 -0500

    Added library dirs

--- sparseqr/sparseqr_gen.py
+++ sparseqr/sparseqr_gen.py
@@ -16,7 +16,8 @@ ffibuilder.set_source( "sparseqr._sparseqr",
     ## You may need to modify the following line,
     ## which is needed on Ubuntu and harmless on Mac OS.
     include_dirs = [ '/opt/homebrew/include/suitesparse' ],
-    libraries = [ '/opt/homebrew/lib' ])
+    library_dirs = [ '/opt/homebrew/lib' ],		
+    libraries = [ 'spqr' ])
 
 ffibuilder.cdef("""
 // The int... is a magic thing which tells the compiler to figure out what the right
